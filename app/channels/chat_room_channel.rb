09e295463998b06133782f6c65480ade32abd465
yujiro fujiwara <imp.yfujiwara@gmail.com>
2019-06-03 02:06:28 +0900

    コメントを追加

--- app/channels/chat_room_channel.rb
+++ app/channels/chat_room_channel.rb
@@ -1,18 +1,30 @@
 class ChatRoomChannel < ApplicationCable::Channel
+  # コンシューマーがこのチャネルのサブスクライバ側になると
+  # このコードが呼び出される
   def subscribed
-    stream_for "chat_room_#{params[:room_id]}"
+    # ストリーム（stream）は、ブロードキャストでパブリッシュするコンテンツをサブスクライバ側にルーティングする機能をチャネルに提供します。
+    # http://laithazer.com/blog/2017/03/25/rails-actioncable-stream-for-vs-stream-from/
+    # stream_from: expects a string
+    # stream_for: expects an object
+    stream_from "chat_room_#{params[:room_id]}"
+    # stream_for "chat_room_#{params[:room_id]}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
+  # return this.perform('speak', {.... のフロントのアクションがトリガー
   def speak(data)
     message = ChatMessage.create(
       chat_room_id: data['room_id'],
       user_id: data['user_id'],
       message: data['message']
     )
+    # broadcastとbroadcast_toの違いは？
+    # broadcastではエラー
+    # broadcast_toでchat_room_#{}に返して
+    # サブスクライバーに届く
     ChatRoomChannel.broadcast_to(
       "chat_room_#{data['room_id']}",
       content: render_message(message)
