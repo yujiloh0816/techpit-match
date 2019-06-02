c1392a0f9aa8b5c5d354cbf0fdde1eb50ad9108d
yujiro fujiwara <imp.yfujiwara@gmail.com>
2019-06-02 16:26:06 +0900

    chatコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :reactions, only: [:create]
   resources :matching, only: [:index]
-
+  resources :chat, only: [:create, :show]
 end
